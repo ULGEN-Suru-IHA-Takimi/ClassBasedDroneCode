76220b9fda1352a3eb1a56cacd807b1834fd4fd4
YousefHUT <YousefHUT>
2025-07-19 03:44:04 +0300

    Hıhı

--- controllers/drone_controller.py
+++ controllers/drone_controller.py
@@ -7,7 +7,8 @@ import json
 import os
 import importlib.util
 from mavsdk import System, telemetry, offboard, action
-
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Kendi modüllerimizi import ediyoruz
 # Proje kök dizininden import etmek için sys.path'e ekleme yapılabilir
 # Ancak modüler yapıda olduğu için göreceli importlar tercih edilir.
